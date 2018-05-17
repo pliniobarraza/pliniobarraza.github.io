5c8944bad43650172dfe52e0e56478ee0576e8f8
Plinio Barraza <barrazaplinio@gmail.com>
2018-05-17 18:49:22 -0500

    hmm. how did this revert: firebase.js in firebase-messging-sw

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,6 +2,7 @@
 // importScripts('/__/firebase/3.8.0/firebase-messaging.js');
 // importScripts('/__/firebase/init.js');
 
+importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase.js');
 importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase-messaging.js');
 // importScripts('https://www.gstatic.com/firebasejs/init.js');
