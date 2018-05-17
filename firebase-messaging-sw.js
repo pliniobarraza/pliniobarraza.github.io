7220bac8a7aa5fa14264b9a978935ef22334e086
Plinio Barraza <barrazaplinio@gmail.com>
2018-05-17 17:48:16 -0500

    updated url for firebase/3.8.0/firebase-app.js in firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,12 @@
-importScripts('/__/firebase/3.8.0/firebase-app.js');
-importScripts('/__/firebase/3.8.0/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+// importScripts('/__/firebase/3.8.0/firebase-app.js');
+// importScripts('/__/firebase/3.8.0/firebase-messaging.js');
+// importScripts('/__/firebase/init.js');
+
+importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase/5.0.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase/5.0.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase/init.js');
+
+
+https://www.gstatic.com/firebasejs/5.0.2/
 
 firebase.messaging();
