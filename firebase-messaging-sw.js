b8d0c909115e0873c95d0c0222ac48cf21fa3f34
Plinio Barraza <barrazaplinio@gmail.com>
2018-05-17 18:04:15 -0500

    replaced init script load for its contents firebase-messaging-sw

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,6 +7,21 @@ importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase-messaging.js');
 // importScripts('https://www.gstatic.com/firebasejs/init.js');
 
 
+//could not find this init script on the gstatic so I am including its contents here 
+
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyBOdfDbflfmAXcgzsUaHjyfJ3njWO8k9XY",
+  "databaseURL": "https://friendlychat-ede27.firebaseio.com",
+  "storageBucket": "friendlychat-ede27.appspot.com",
+  "authDomain": "friendlychat-ede27.firebaseapp.com",
+  "messagingSenderId": "108673168113",
+  "projectId": "friendlychat-ede27"
+});
+
+
+
+
 https://www.gstatic.com/firebasejs/5.0.2/
 
 firebase.messaging();
