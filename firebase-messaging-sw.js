31452c41f879757763e41162677afc833f711b3e
Plinio Barraza <barrazaplinio@gmail.com>
2018-05-17 17:50:22 -0500

    removed small typo in prev commit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,8 +2,8 @@
 // importScripts('/__/firebase/3.8.0/firebase-messaging.js');
 // importScripts('/__/firebase/init.js');
 
-importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase/5.0.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase/5.0.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase-messaging.js');
 importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase/init.js');
 
 
