9ea794c963d8ad261f4b5cd0adb16f66bb335289
Plinio Barraza <barrazaplinio@gmail.com>
2018-05-17 19:01:42 -0500

    downgraded firebase 5.0.2 to 5.0.1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,9 +2,9 @@
 // importScripts('/__/firebase/3.8.0/firebase-messaging.js');
 // importScripts('/__/firebase/init.js');
 
-importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase.js');
-importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.0.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.1/firebase.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.1/firebase-messaging.js');
 // importScripts('https://www.gstatic.com/firebasejs/init.js');
 
 
